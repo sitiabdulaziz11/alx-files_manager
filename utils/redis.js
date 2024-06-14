549a9de3ba2d9f65ce2a99fbf6b57e3d268c53fb
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-06-14 14:13:36 +0300

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,47 @@
+const redis = require('redis');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        this.client.on('error', (err) => {
+            console.log('Redis Client Error', err);
+        })
+    }
+    isAlive() {
+        return this.client.connected;
+    }
+    async get(key) {
+        return new Promise((resolve, reject) => {
+          this.client.get(key, (err, value) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(value);
+          });
+        });
+      }
+      
+      async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+          this.client.set(key, value, 'EX', duration, (err) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(true);
+          });
+        });
+      }
+      async del(key) {
+        return new Promise((resolve, reject) => {
+          this.client.del(key, (err) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(true);
+          });
+        });
+      }       
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
